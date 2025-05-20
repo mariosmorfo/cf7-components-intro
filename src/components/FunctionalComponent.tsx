3b65051811c49acf5580cdc1fc220cc29d4bd476
Marios.Morfonidis <marios.morfonidis98@gmail.com>
2025-05-20 21:02:41 +0300

    feat: new components created

---
+++ src/components/FunctionalComponent.tsx
@@ -0,0 +1,7 @@
+function FunctionalComponent() {
+    const title = "Is functional component";
+    return <h1 className="text-center mt-12 font-bold">{title}</h1>
+
+}
+
+export default FunctionalComponent;
