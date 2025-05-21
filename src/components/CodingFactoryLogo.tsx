6074abe704a6d0d49cba9a82a7311d4729f71709
Marios.Morfonidis <marios.morfonidis98@gmail.com>
2025-05-21 20:19:21 +0300

    feat: Coding Factory Logo

---
+++ src/components/CodingFactoryLogo.tsx
@@ -0,0 +1,12 @@
+const CodingFactoryLogo = () => {
+    return(
+        <>
+            <img
+                className="h-16 my-4"
+                src="/logo.png" alt="CF7"/>
+        
+        </>
+    )
+}
+
+export default CodingFactoryLogo;
