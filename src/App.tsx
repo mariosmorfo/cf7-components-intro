7de7ae930e73764457efeef8c239c8c081e7094a
Marios.Morfonidis <marios.morfonidis98@gmail.com>
2025-05-22 12:15:27 +0300

    feat: Implements Layout and Counter components

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,31 @@
-import ClassComponent from "./components/ClassComponent.tsx";
-import FunctionalComponent from "./components/FunctionalComponent.tsx";
-import ArrowFunctionalComponents from "./components/ArrowFunctionalComponents.tsx";
+// import ClassComponent from "./components/ClassComponent.tsx";
+// import ArrowFunctionalComponents from "./components/ArrowFunctionalComponents.tsx";
+// import ArrowFunctionalComponentsWithProps from "./components/ArrowFunctionalComponentsWithProps.tsx";
+// import ArrowFunctionalComponentsWithPropsType from "./components/ArrowFunctionalComponentsWithPropsType.tsx";
+// import CodingFactoryLogo from "./components/CodingFactoryLogo.tsx";
+import Layout from "./components/Layout.tsx";
+// import FunctionalComponentWithState from "./components/FunctionalComponentWithState.tsx";
+import Counter from "./components/Counter.tsx";
+// import ClassComponentWithState from "./components/ClassComponentWithState.tsx";
+// import FunctionalComponent from "./components/FunctionalComponent.tsx";
 
-import ArrowFunctionalComponentsWithProps from "./components/ArrowFunctionalComponentsWithProps.tsx";
-import ArrowFunctionalComponentsWithPropsType from "./components/ArrowFunctionalComponentsWithPropsType.tsx";
 function App() {
 
   return (
     <>
-    <ClassComponent/>
-        <FunctionalComponent/>
-        <ArrowFunctionalComponents/>
-        <ArrowFunctionalComponentsWithProps title="Is arrow functional component with props"/>
-        <ArrowFunctionalComponentsWithPropsType title="Is arrow functional component with props" description="This is description"/>
+    {/*<ClassComponent/>*/}
+    {/*    <FunctionalComponent/>*/}
+    {/*    <ArrowFunctionalComponents/>*/}
+    {/*    <ArrowFunctionalComponentsWithProps title="Is arrow functional component with props"/>*/}
+    {/*    <ArrowFunctionalComponentsWithPropsType title="Is arrow functional component with props" description="This is description"/>*/}
+    {/*    <CodingFactoryLogo/>*/}
+        <Layout>
+            {/*<h1 className="text-center text-2xl font-bold">This is a h1 title</h1>*/}
+            {/*<FunctionalComponent/>*/}
+            {/*<ClassComponentWithState/>*/}
+            {/*<FunctionalComponentWithState/>*/}
+            <Counter/>
+        </Layout>
     </>
   )
 }
