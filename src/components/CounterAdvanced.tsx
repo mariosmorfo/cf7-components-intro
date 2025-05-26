988481fd028168417941e293adcdec7acf8e7ccf
Marios.Morfonidis <marios.morfonidis98@gmail.com>
2025-05-26 20:28:05 +0300

    feat: new component advanced created

---
+++ src/components/CounterAdvanced.tsx
@@ -0,0 +1,58 @@
+import CounterButton from "./CounterButton.tsx";
+import {useState} from "react";
+
+type CounterState = {
+    count: number;
+    lastAction: string;
+    time: string;
+}
+
+const CounterAdvanced = () => {
+    const [state, setState] = useState<CounterState>({
+        count: 0,
+        lastAction: "",
+        time: ""
+    })
+
+    const increaseCount = () => {
+        setState({
+            count: state.count + 1,
+            lastAction: "Increase",
+            time: new Date().toLocaleTimeString()})
+    }
+
+    const decreaseCount = () => {
+        if(state.count > 0) {
+            setState({
+                count: state.count -1,
+                lastAction: "Decrease",
+                time: new Date().toLocaleTimeString()})
+        }}
+
+    const resetCount = () => {
+        setState({
+            count: 0,
+            lastAction: "Reset",
+            time: new Date().toLocaleTimeString()
+        })
+    }
+
+
+
+    return(
+        <>
+
+            <div className="space-y-4 pt-12 ">
+                <h1 className="text-center">Count is {state.count}</h1>
+                <div className="text-center space-x-4">
+                    <CounterButton onClick={increaseCount} label="Increase"/>
+                    <CounterButton onClick={decreaseCount} disabled={state.count === 0} label="Decrease"/>
+                    <CounterButton onClick={resetCount} disabled={ state.count=== 0} label="Reset" addClass="bg-cf-dark-red" />
+                </div>
+            </div>
+            <p className="text-center pt-8">Last change: <strong>{state.lastAction || "_"}</strong> at {state.time}</p>
+        </>
+    )
+}
+
+export default CounterAdvanced;
