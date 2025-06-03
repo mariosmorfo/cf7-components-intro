818cc51cc06a39e4497f0d2c7d33e039fa2c1416
Marios.Morfonidis <marios.morfonidis98@gmail.com>
2025-06-03 21:11:24 +0300

    feat: Create HomePage to navigate a message

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,8 @@
+const HomePage = () => {
+    return(
+        <>
+            <h1 className="text-bold text-black text-center">Hello Coding Factory</h1>
+        </>
+    )
+}
+export default HomePage;
