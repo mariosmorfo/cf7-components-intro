9eeac325b436962b12c0a9fd258338bf35122f7e
Marios.Morfonidis <marios.morfonidis98@gmail.com>
2025-06-04 19:47:14 +0300

    feat: create responsive header

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,17 +1,19 @@
-import Header from "./Header.tsx";
+import React from "react";
+// import Header from "./Header.tsx";
 import Footer from "./Footer.tsx";
+import HeaderResponsive from "./HeaderResponsive.tsx";
 
-
-interface LayoutProps {
+interface LayoutProps{
     children: React.ReactNode;
 }
 
-const Layout = ({children}: LayoutProps) => {
-    return(
+const Layout = ({children}:LayoutProps) => {
+    return (
         <>
-          <Header/>
-            <div className="container mx-auto min-h-[85vh] pt-24">
-            {children}
+            {/*<Header/>*/}
+            <HeaderResponsive/>
+            <div className="container mx-auto min-h-[95vh] pt-24">
+                {children}
             </div>
             <Footer/>
         </>
