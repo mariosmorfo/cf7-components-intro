1a16d19aed55c68054b41a998d6c13cad9c8d920
Marios.Morfonidis <marios.morfonidis98@gmail.com>
2025-06-16 21:01:49 +0300

    feat: import custom button from shadcn library

--- src/components/MultiFieldFormWithReactHook.tsx
+++ src/components/MultiFieldFormWithReactHook.tsx
@@ -1,6 +1,7 @@
 import {z} from "zod";
 import {useForm} from "react-hook-form";
 import {zodResolver} from "@hookform/resolvers/zod";
+import {Button} from "@/components/ui/button.tsx";
 
 const formSchema = z.object({
     name: z.string().trim().nonempty("Name is required."),
@@ -97,6 +98,7 @@ const MultiFieldFormWithReactHook = () => {
                             className="bg-gray-200 text-cf-gray-700 px-4 py-2 rounded">
                             Clear
                         </button>
+                        <Button>Click me</Button>
                     </div>
                 </form>
                 <div className="mt-6 border-t pt-4 space-y-2">
